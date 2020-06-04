5f7157a36c7ae089bd4349ad5e867e2aa5692060
crpistillo <cpistillo@fi.uba.ar>
2020-06-04 06:11:56 -0300

    Arboles Binarios de Busqueda (BST)

---
+++ cmake-build-debug/CMakeFiles/ABB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "ABB"
+  "ABB.pdb"
+  "CMakeFiles/ABB.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ABB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
