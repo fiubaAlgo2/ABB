e80327e5bd15784295692c791cf1142a2cc495d3
crpistillo <cpistillo@fi.uba.ar>
2020-06-04 13:59:59 -0300

    .

---
+++ include/Cola.cpp
@@ -0,0 +1,82 @@
+//
+//  Cola.cpp
+//  EjListas
+//
+//  Created by Carolina Pistillo on 16/10/18.
+//  Copyright © 2018 Carolina Pistillo. All rights reserved.
+//
+
+#include "Cola.h"
+
+Cola::Cola()
+{
+    this->primer_nodo=0;
+    this->ultimo_nodo=0;
+    this->tamanio = 0;
+}
+
+Cola::~Cola()
+{
+    while(!(this->cola_vacia()))
+    {
+        this->Desencolar();
+    }
+}
+
+void Cola::Encolar(int entero)
+{
+    Nodo* ptr_nodo = new Nodo(entero);
+    
+    if(cola_vacia())
+    {
+        this->primer_nodo=ptr_nodo;
+    }
+    
+    else
+    {
+        this->ultimo_nodo->set_siguiente(ptr_nodo);
+    }
+    this->ultimo_nodo=ptr_nodo;
+    this->tamanio++;
+}
+
+int Cola::Desencolar()
+{
+    int datoDesacolado=0;
+    
+    if(!cola_vacia())
+    {
+        Nodo* ptr_aux = this->primer_nodo;
+        
+        this->primer_nodo = ptr_aux->get_siguiente();
+        
+        datoDesacolado = ptr_aux->get_dato();
+        delete ptr_aux;
+    }
+    this->tamanio--;
+    return datoDesacolado;
+}
+
+int Cola::get_primeroEnCola()
+{
+    if(!cola_vacia())
+        return primer_nodo->get_dato();
+    else return 0;
+}
+
+bool Cola::cola_vacia()
+{
+    return(this->primer_nodo==0);
+}
+
+unsigned int Cola::get_tamanio()
+{
+    return this->tamanio;
+}
+
+
+
+
+
+
+
